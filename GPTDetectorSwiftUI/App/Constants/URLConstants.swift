ed39a0b3cc1e04acb77a19df45b259c4a0fff35d
cem256 <cemo99@gmail.com>
2024-04-13 14:13:52 +0300

    update metadate and app url

--- GPTDetectorSwiftUI/App/Constants/URLConstants.swift
+++ GPTDetectorSwiftUI/App/Constants/URLConstants.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct URLConstants {
     private init() {}
-    static let appStoreUrl: URL = .init(string: "https://apps.apple.com/app/fabi-ai-detector/id6483928654")!
+    static let appStoreUrl: URL = .init(string: "https://apps.apple.com/app/fabi-ai-detector/id6466612799")!
     static let contributionUrl: URL = .init(string: "https://github.com/cem256/AI-Detector-iOS")!
     static let privacyPolicyUrl: URL = .init(string: "https://cem256.github.io/AI-Detector-iOS/privacy-policy.html")!
 }
